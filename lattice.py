f38572f002f97bfb08d2fb5a32d621b63cc0a77b
OlavKvalbein <olav.kvalbein03@gmail.com>
2026-02-28 14:54:06 +0100

    added 0-spin boundary and actually correct simulation (hopefully)

--- lattice.py
+++ lattice.py
@@ -6,7 +6,6 @@ class Lattice():
     def __init__(self, L, Lz, T_J_ratio, Jz_J_ratio):
         self.L = L
         self.Lz = Lz
-        self.shape = (Lz, L, L)
         self.J_T_ratio = 1/T_J_ratio
         self.Jz_J_ratio = Jz_J_ratio
 
@@ -14,7 +13,12 @@ class Lattice():
         self.spin = self.init_spin()
 
     def init_spin(self):
-        return self.rng.choice([np.int8(-1),np.int8(1)], size=self.shape)
+        # (Lz+2,L+2,L+2) array of random spin in the interior and boundary of 0-spin
+        spin = self.rng.choice([-1,1], size=(self.Lz+2, self.L+2, self.L+2))
+        spin[0,:,:] = spin[-1,:,:] = 0
+        spin[:,0,:] = spin[:,-1,:] = 0
+        spin[:,:,0] = spin[:,:,-1] = 0
+        return spin
 
     # update grid by one MC_step
     def MC_step(self):
@@ -22,80 +26,60 @@ class Lattice():
             self.step()
 
     def step(self):
-        # pick random point and cardinal direction of a random neighbor in the plane
-        x, y, z, nesw = self.random_opposite_neighbors()
-        deltaE = self.energy_diff(z, x1, y1, x2, y2) # acually DeltaE/T
+        x1, y1, x2, y2, z = self.random_opposite_neighbors()
+        deltaE = self.energy_diff(x1, y1, x2, y2, z) # acually DeltaE/T
         if deltaE < 0:
-            self.exchange_spins(z, x1, y1, x2, y2)
+            self.exchange_spins(x1, y1, x2, y2, z)
         else:
             exchange_probability = np.exp(-deltaE)
             if self.rng.random() < exchange_probability:
-                self.exchange_spins(z, x1, y1, x2, y2)
+                self.exchange_spins(x1, y1, x2, y2, z)
 
-    def random_opposite_neighbors(self): # in the same xy-plane
+    def random_opposite_neighbors(self):
         for _ in range(self.MAX_TRIES_SELECTING):
-            z = self.rng.integers(self.Lz)
-            x1, y1 = self.rng.integers(0, self.L, 2)
-            x2, y2 = self.random_neighbor(x1, y1)
-            if self.spin[z,y1,x1] != self.spin[z,y2,x2]:
-                return z, x1, y1, x2, y2
+            # generate random point not on the 0-spin boundary
+            z = self.rng.integers(1,self.Lz+1)
+            x1, y1 = self.rng.integers(1, self.L+1, 2)
 
-    def random_neighbor(self, x1, y1):
-        x2 = x1
-        y2 = y1
-        if 0 < x1 < self.L - 1:
-            if 0 < y1 < self.L - 1:
-                choice = self.rng.integers(4)
-                if choice == 0: x2 -= 1
-                elif choice == 1: x2 += 1
-                elif choice == 2: y2 -= 1
-                elif choice == 3: y2 += 1
+            # generate neighbor possibly on the boundary
+            offset = self.rng.choice([-1,1])
+            if self.rng.random() < 0.5:
+                x2 = x1 + offset
+                y2 = y1
             else:
-                choice = self.rng.integers(3)
-                if choice == 0: x2 -= 1
-                elif choice == 1: x2 += 1
-                elif choice == 2:
-                    y2 = 1 if y1 == 0 else self.L - 2
-        else:
-            if 0 < y1 < self.L - 1:
-                choice = self.rng.integers(3)
-                if choice == 0:
-                    x2 = 1 if x1 == 0 else self.L - 2
-                elif choice == 1: y2 -= 1
-                elif choice == 2: y2 += 1
-            else:
-                choice = self.rng.integers(2)
-                if choice == 0:
-                    x2 = 1 if x1 == 0 else self.L - 2
-                if choice == 1:
-                    y2 = 1 if y1 == 0 else self.L - 2
-        return x2, y2
+                x2 = x1
+                y2 = y1 + offset
 
-    def energy_diff(self, x, y, z, nesw):
+            if self.spin[z,y1,x1] == -self.spin[z,y2,x2]:
+                return x1, y1, x2, y2, z
+        print("could not find random opposite neighbors")
+
+    def energy_diff(self, x1, y1, x2, y2, z):
         s1 = self.spin[z,y1,x1]
-        N1 = self.nesw_sum[z,y1,x1]
-        N2 = self.nesw_sum[z,y2,x2]
-        M1 = self.ud_sum[z,y1,x1]
-        M2 = self.ud_sum[z,y2,x2]
-        return -self.J_T_ratio*2*s1*(N2-N1-2*s1+self.Jz_J_ratio*(M2-M1))
+        N1 = self.nesw_sum(x1, y1, z)
+        N2 = self.nesw_sum(x2, y2, z)
+        U1 = self.spin[z+1,y1,x1] + self.spin[z-1,y1,x1]
+        U2 = self.spin[z+1,y2,x2] + self.spin[z-1,y2,x2]
+        return self.J_T_ratio*2*s1*(N1 - N2 + 2*s1 + self.Jz_J_ratio*(U1 - U2))
+                
+    def nesw_sum(self, x, y, z):
+        return self.spin[z,y,x+1] + self.spin[z,y,x-1] + self.spin[z,y+1,x] + self.spin[z,y-1,x]
 
-    def exchange_spins(self, z, x1, y1, x2, y2):
-        # update neighbor sums
-        self.nesw_sum[z,y1,x1] += 2*self.spin[z,y1,x1]
-        self.nesw_sum[z,y2,x2] -= 2*self.spin[z,y1,x1]
-
+    def exchange_spins(self, x1, y1, x2, y2, z):
         self.spin[z,y1,x2] *= -1
         self.spin[z,y2,x2] *= -1
-
+    
+    def get_spin(self):
+        return self.spin[1:-1,1:-1,1:-1].copy()
 
 def lattice_series(L, Lz, T_J_ratio, Jz_J_ratio, MC_steps):
     lattice = Lattice(L, Lz, T_J_ratio, Jz_J_ratio)
-    series = [lattice.spin.copy()]
+    series = [lattice.get_spin()]
 
     for i in range(MC_steps):
         print(f"\rGenerating lattice series... MC step {i+1}/{MC_steps}", end="", flush=True)
         lattice.MC_step()
-        series.append(lattice.spin.copy())
+        series.append(lattice.get_spin())
     return series
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -4,19 +4,20 @@ from lattice import lattice_series
 
 def plot_series():
     L = 20
-    Lz = 3
-    T_J_ratio = 0.1
-    Jz_J_ratio = 0.1
+    Lz = 5
+    T_J_ratio = 1.5
+    Jz_J_ratio = 0.5
 
-    show_after_steps = [0, 10, 100]
+    show_after_steps = [0, 50, 100, 150, 200]
     MC_steps = max(show_after_steps)
 
     series = lattice_series(L, Lz, T_J_ratio, Jz_J_ratio, MC_steps)
     fig, ax = plt.subplots(nrows=Lz, ncols=len(show_after_steps))
+    fig.supylabel("z")
     for j in range(len(show_after_steps)):
+        ax[0][j].set_title(f"MC steps = {show_after_steps[j]}")
         for i in range(Lz):
             ax[i][j].imshow(series[show_after_steps[j]][i,:,:], cmap="gray")
-            ax[i][j].set_title(f"MC steps = {show_after_steps[j]}")
     plt.show()
 
 plot_series()
