4b5ab20d10e24a64df5c9c128d8fa78a12da0404
OlavKvalbein <olav.kvalbein03@gmail.com>
2026-04-16 13:33:24 +0200

    completed sampling stuff

--- c/main.c
+++ c/main.c
@@ -8,13 +8,10 @@
 
 typedef struct
 {
-	int nr_steps;
+	int n_steps;
+	int n_samples;
 	int burn_in_steps;	
-	int sample_period;
-	int nr_samples;
-
-	bool sample_spin;
-
+	
 	bool sample_specific_heat;
 	double* H;       // List of hamiltonian at each step
 	double* H_sq;	// List of hamiltonian^2 at each step
@@ -23,42 +20,58 @@ typedef struct
 	// something for delta^2
 } SamplingData;
 
-SamplingData new_sampling_data(
-	int nr_steps, int burn_in_steps, int sample_period, 
-	bool sample_specific_heat, bool sample_interconnectivity
-){
-	int nr_samples = (nr_steps - burn_in_steps) / sample_period; // not correct
+// allocates the space for variables sampled
+void init_sampling_data(SamplingData* data)
+{	
+	if (data->sample_specific_heat) {
+		double *H = (double*)malloc(data->n_samples*sizeof(double));
+		double *H_sq = (double*)malloc(data->n_samples*sizeof(double));
+	}
 
-	double *H = (double*)malloc(nr_samples*sizeof(double));
-	double *H_sq = (double*)malloc(nr_samples*sizeof(double));
+	if (data->sample_interconnectivity) {
+		// TODO
+	}
 }
 
 // samples the lattice for the relevant variables at a given sample number
 // and adds it to the totals
-void sample(const Lattice* lat, int sample_nr, SamplingData* data)
+void sample_lattice(const Lattice* lat, int sample_nr, SamplingData* data)
 {
-	double hamil = energy(lat);
-	data->H[sample_nr] += hamil; 
-	data->H_sq[sample_nr] += hamil*hamil;
+	if (data->sample_specific_heat) {
+		double hamil = energy(lat);
+		data->H[sample_nr] += hamil; 
+		data->H_sq[sample_nr] += hamil*hamil;
+	}
+
+	if (data->sample_interconnectivity) {
+		// TODO
+	}
 }
 
 // runs an ensemble of lattices to some given steps and samples them underways.
 void run_ensemble(Lattice* lat, int ensemble_size, SamplingData* data)
 {
+	init_sampling_data(data);
+
 	for (int i = 0; i < ensemble_size; i++) {
 		reset_lattice(lat);
-		int sample_nr = 0;
-		for (int step = 0; step < data->nr_steps; step++) {
-			// are we to sample?
-			if (step >= data->burn_in_steps && step % data->sample_period == 0) {
-				sample(lat, sample_nr, data);
-				sample_nr++;
-			}
+
+		// do the burn in steps
+		for (int step = 0; step < data->burn_in_steps; step++)
 			do_step(lat);
-		} // this for-loop potentially misses the last sample. 
+
+		// sampling
+		sample_lattice(lat, 0, data);
+		int sample_period = (data->n_steps - data->burn_in_steps)/data->n_samples;
+		for (int sample_nr = 1; sample_nr < data->n_samples; sample_nr++) {
+			for (int i = 0; i < sample_period; i++)
+				do_step(lat);
+			sample_lattice(lat, sample_nr, data);
+		}
 	}
+
 	// divide each sampled variable by ensemble size to get ensemble averages.
-	for (int i = 0; i < data->nr_samples; i++) {
+	for (int i = 0; i < data->n_samples; i++) {
 		data->H[i] /= ensemble_size;
 		data->H_sq[i] /= ensemble_size;
 	}
@@ -74,10 +87,22 @@ int main()
 	double Jz = 0.5;
 	Lattice lat = new_lattice(L, Lz, T, Jz);
 
-	int steps = 100;
-	for (int i = 0; i < steps; i++) {
-		do_step(&lat);
-	}
+	SamplingData sampling_data = {
+		.n_steps = 1000,
+		.n_samples = 100,
+		.burn_in_steps = 0,
+		.sample_specific_heat = true,
+		.sample_interconnectivity = false,
+	};
+
+	run_ensemble(&lat, 10, &sampling_data);
+
+	
+
+	// int steps = 100;
+	// for (int i = 0; i < steps; i++) {
+	// 	do_step(&lat);
+	// }
 
 	// char after_path[100];
 	// snprintf(after_path, sizeof(after_path),
