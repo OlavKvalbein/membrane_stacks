2cdc5e807aa22120a39c42623572463aec336a7d
alextry004 <alextry004@gmail.com>
2026-04-26 16:39:37 +0200

    Added documents

--- plotting/energy_plot.py
+++ plotting/energy_plot.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv("data/sampling.csv", comment="#", index_col=False)
+df = pd.read_csv("data/energy.csv", comment="#", index_col=False)
 
 plt.plot(df["H"])
 plt.show()
