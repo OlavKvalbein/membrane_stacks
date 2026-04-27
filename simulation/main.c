ce650131503b571d21baa9a1b58392aab5065190
OlavKvalbein <olav.kvalbein03@gmail.com>
2026-04-27 16:49:10 +0200

    made more energy timeseries

--- simulation/main.c
+++ simulation/main.c
@@ -12,14 +12,15 @@ int main()
 
 	SamplingData sampling_data = {
 		.ensemble_size = 5,
-		.n_steps = 10'000,
+		.n_steps = 1'000'000,
 		.n_samples = 100,
 		.n_burn_in = 0,
 		.sample_specific_heat = true,
 		.sample_interconnectivity = false,
 	};
 
-	double Ts[] = {1.0, 1.25, 1.5, 1.75, 2.0, 2.25, 2.50};
+	// ensemble averages at different temperatures
+	double Ts[] = {1.0, 1.125, 1.25};
 	char filepath[200];
 	for (int i = 0; i < sizeof(Ts)/sizeof(double); i++) {
 		lat.T = Ts[i];
